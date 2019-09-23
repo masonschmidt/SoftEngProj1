4b5d67602d9afb3fdbbb5e53c893d7789baa3bd3
ClayGlen <Glen5641@ou.edu>
2019-09-23 00:19:19 -0500

    Final Version

--- RandomTiming.py
+++ RandomTiming.py
@@ -5,22 +5,30 @@ import time
 
 if __name__ == "__main__":
     ## Request HTML text from Database Page and Load in YAML
-    res = requests.get('https://glen5641.github.io/YAMLEXAML/')
-    yamlfile = res.text[res.text.find("<pre>")+5:res.text.find("</pre>")]
+    res = requests.get('https://masonschmidt.github.io/SoftEngProj1/')
+    yamlfile = res.text[res.text.find("<pre>")+1:res.text.find("</pre>")]
 
     ## Use YAML library to parse the YAML into an object
     data = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
-    ## Step through each IP Address recording Time
-    start = time.time()
-    for i in data['ipaddresses']:
+    total_start = time.time()
+
+    for i in data['addresses']:
+
+        ## Step through each IP Address recording Time
+        start = time.time()
+
         res = requests.get(i)
 
         ## Get and Print the Random Number
         result = res.text[(res.text.find("<h1>")+4):(res.text.find("</h1>"))]
         print(i + " :: " + result)
 
-    ## Collect final time in Milliseconds
-    ## and print difference in seconds
-    finish = time.time()
-    print(str((finish - start)/1000) + ' seconds')
+        ## Collect final time in Milliseconds
+        ## and print difference in seconds
+        finish = time.time()
+        print('Runtime is ' + str((finish - start)/1000) + ' seconds')
+        print()
+
+    total_finish = time.time()
+    print('Total Runtime of the Program is ' + str((total_finish - total_start)/1000) + ' seconds')
